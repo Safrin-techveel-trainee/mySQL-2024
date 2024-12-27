4914358c38c4e0c67ae7194f7a8c92ea52ee733e
S-Safrin <133481766+S-Safrin@users.noreply.github.com>
2024-12-27 11:14:19 +0530

    commit

--- script.sql
+++ script.sql
@@ -1,1 +1,11 @@
--- task 
+create  table employee(
+employee_id int primary key,
+name varchar(50) not null,
+dept varchar(60) not null,
+salary int unsigned not null
+);
+select * from employee;
+alter table employee add(position varchar(50) not null);
+alter table employee modify column salary decimal(10,2);
+alter table employee rename column dept to department;
+alter table employee drop column position;
